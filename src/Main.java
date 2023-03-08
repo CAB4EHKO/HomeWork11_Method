edb28bfdf2b4ab37aa0edae9e809f13dcfa217bc
Savchenko <savchenko.ra@yandex.ru>
2023-03-09 00:48:54 +0300

    Task 2 + correct task 1.

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,46 @@
-public class Main {
-    public static void main(String[] args) {
-        task1();
-    }
+import java.time.LocalDate;
 
+public class Main {
     public static void showsLeapYearOrNot(int thisYear) {
-        if (thisYear % 4 != 0 || thisYear % 100 == 0) {
-            System.out.println(thisYear + " год - невисокосный.");
-        } else if (thisYear % 4 == 0 && thisYear % 100 != 0) {
+        if (thisYear % 4 == 0 && (thisYear % 100 != 0 || thisYear % 400 == 0)) {
             System.out.println(thisYear + " год - високосный.");
+        } else {
+            System.out.println(thisYear + " год - невисокосный.");
         }
     }
+
+    public static void checkDevice(int deviceOS, int deviceYear) {
+        int currentYear = LocalDate.now().getYear();
+        if (deviceOS == 0) {
+            if (deviceYear < currentYear) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+            } else {
+                System.out.println("Установите полную версию приложения для iOS по ссылке.");
+            }
+        } else if (deviceOS == 1) {
+            if (deviceYear < currentYear) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+            } else {
+                System.out.println("Установите полную версию приложения для Android по ссылке.");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        task1();
+        task2();
+    }
+
+
     public static void task1() {
         System.out.println("Task 1.");
-        int year = 2003;
-        showsLeapYearOrNot(year);
+        showsLeapYearOrNot(1900);
+    }
+
+    public static void task2() {
+        System.out.println("Task 2.");
+        int deviceOS = 0;
+        int deviceYeaOfIssue = 2023;
+        checkDevice(deviceOS, deviceYeaOfIssue);
     }
 }
