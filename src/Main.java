3b695073c8ee798d212c6907cd769730e4c08085
Savchenko <savchenko.ra@yandex.ru>
2023-03-08 23:00:22 +0300

    Task 1.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+
+    public static void showsLeapYearOrNot(int thisYear) {
+        if (thisYear % 4 != 0 || thisYear % 100 == 0) {
+            System.out.println(thisYear + " год - невисокосный.");
+        } else if (thisYear % 4 == 0 && thisYear % 100 != 0) {
+            System.out.println(thisYear + " год - високосный.");
+        }
+    }
+    public static void task1() {
+        System.out.println("Task 1.");
+        int year = 2003;
+        showsLeapYearOrNot(year);
     }
 }
